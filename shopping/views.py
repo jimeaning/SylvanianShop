f27744b3e221f466f4f10dbcc9297bbfc0ad680b
hiroong <kjimin63@naver.com>
2021-12-17 01:21:56 +0900

    포스트 목록과 상세 페이지에 카테고리 기능 추가

--- shopping/views.py
+++ shopping/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from .models import Post
+from .models import Post, Category
 
 class PostList(ListView) :
     model = Post
-    ordering = '-pk'
     # paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostList, self).get_context_data()
-        # context['categories'] = Category.objects.all()
-        # context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 #    template_name = 'blog/post_list.html'
 
@@ -20,8 +19,8 @@ class PostDetail(DetailView) :
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostDetail, self).get_context_data()
-        # context['categories'] = Category.objects.all()
-        # context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
         # context['comment_form'] = CommentForm
         return context
 
