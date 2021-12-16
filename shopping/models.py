84042de19a30534ad4d8a611bc18378980024d2d
hiroong <kjimin63@naver.com>
2021-12-16 23:14:06 +0900

    포스트 목록과 상세 페이지 부트스트랩 적용

--- shopping/models.py
+++ shopping/models.py
@@ -3,12 +3,14 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
+    content = models.TextField(default = '')
 
+    head_image = models.ImageField(upload_to='shopping/images/%Y/%m/%d/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title} :: {self.author}'
+        return f'[{self.pk}]{self.title}'
 
     def get_absolute_url(self):
         return f'/shop/{self.pk}/'
