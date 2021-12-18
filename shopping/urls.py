3fdc074665fc6416f8fc5c5b0a2665c97d07f4b3
hiroong <kjimin63@naver.com>
2021-12-19 01:24:13 +0900

    폼으로 상세 페이지에 댓글 작성하기

--- shopping/urls.py
+++ shopping/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('create_prod/', views.PostCreate.as_view()),
     path('category/<str:slug>', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('<int:pk>/new_comment/', views.new_comment),
     path('', views.PostList.as_view()),
 ]
